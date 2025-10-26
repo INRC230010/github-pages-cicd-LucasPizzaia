a90dfda253a7160905f317ac630c4591b817e871
LucasPizzaia <lucas.pizzaia@edu.unifil.br>
2025-10-26 15:46:51 -0300

    commit site.url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,7 +11,7 @@ const config: GatsbyConfig = {
     siteTitle: `Emilia`,
     siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
     siteHeadline: `Emilia - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://emilia.lekoarts.de`,
+    siteUrl: `https://lucaspizzaia.github.io`,
     siteDescription: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI. Includes Light/Dark mode.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
